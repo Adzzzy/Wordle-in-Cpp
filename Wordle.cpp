075106022ae19d68dde8a06381f1ab68d6d0c655
Adam Rebes <adam.rebes@hotmail.com>
2023-05-02 13:06:55 +0930

    C++ Wordle Project. First Commit.

---
+++ Wordle.cpp
@@ -0,0 +1,23 @@
+/*******************************/
+/* Wordle in C++ by Adam Rebes */
+/*******************************/
+
+/* This is a project to recreate wordle in c++. Wordle is a word game where the player must guess 
+a 5-letter word in 6 guesses by using feedback given about whether a letter is in the correct
+spot, exists in the word but is in a different spot, or that the letter doesn't exist in the word. 
+The original game is available here: https://www.nytimes.com/games/wordle/index.html
+Unlike in the original, this version will allow guesses that aren't real words. */
+
+int main() {
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
